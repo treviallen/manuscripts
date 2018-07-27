b91c92dae2dcc4430ccac25112ce6c2e7795484d
treviallen <trevor.allen@canada.ca>
2018-07-27 23:21:11 +1000

    script for plotting multiple maps

---
+++ nsha18/final_overview/plt_multi_haz_maps.py
@@ -0,0 +1,427 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 11:47:02 2015
+
+convert gridded hazard to map
+
+Usage:
+    python map_nsha18.py <path to csv file>
+    
+
+@author: tallen
+"""
+from sys import argv
+from matplotlib.mlab import griddata
+from matplotlib import colors, colorbar #, cm
+from os import path, mkdir, getcwd
+#import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from numpy import arange, array, log10, mean, mgrid, ogrid, percentile, ma, isnan, nan, where, delete, floor, ceil
+from tools.mapping_tools import get_map_polygons, mask_outside_polygons, cpt2colormap # drawshapepoly, labelpolygon, 
+import shapefile
+from scipy.constants import g
+import matplotlib.gridspec as gridspec
+
+#from gmt_tools import cpt2colormap
+#from shapely.geometry import Point, Polygon
+
+##############################################################################
+# set some default values here
+##############################################################################
+mpl.use('Agg')
+mpl.style.use('classic')
+mpl.rcParams['pdf.fonttype'] = 42
+
+drawshape = False # decides whether to overlay seismic sources
+
+# set map resolution
+res = 'i' 
+
+# get current working directory (and computer!)
+cwd = getcwd()
+
+##############################################################################
+# define inputs
+##############################################################################
+
+# set with paths to hazard grids - format = plotting name; file path
+paramfile = argv[1]
+
+# which probability - acceptable values are: 2 (2%), 9 (9.5%) or 10 (10%)
+pltProbability = argv[2]
+
+##############################################################################
+# parse param file
+##############################################################################
+
+lines = open(paramfile).readlines()
+hazfiles = []
+modnames = []
+outfile = lines[0].strip()
+for line in lines[1:]:
+    modnames.append(line.strip().split(';')[0])
+    hazfiles.append(line.strip().split(';')[1])
+
+# set figure size based on number of models
+maprows = int(ceil(len(modnames) / 2.))
+figure = plt.figure(1,figsize=(19,9*maprows+1))
+
+gs1 = gridspec.GridSpec(maprows, 2)
+gs1.update(wspace=0.025, hspace=0.05)
+
+pltlett = ['a)', 'b)', 'c)', 'd)', 'e)']
+##############################################################################
+# loop thru files and parse hazard grids
+##############################################################################
+
+for ii, hazfile in enumerate(hazfiles):
+
+    # set axes
+    #ax = figure.add_subplot(maprows, 2, ii+1)
+    ax = figure.add_subplot(gs1[ii])
+    
+    # parse hazard grid file 
+    lines = open(hazfile).readlines()
+    
+    # get keys for model
+    if lines[0].startswith('#'):
+        line = lines[1]
+    else:
+        line = lines[0]
+    
+    # get dictionary keys
+    keys = line.strip().split(',')[2:]
+    
+    # make grid dictionary
+    grddict = []
+    gshap = False
+    print '\nReading', modnames[ii]
+    for line in lines[2:]:
+        dat = line.strip().split(',')
+        
+        tmpdict = {'lon':float(dat[0]), 'lat':float(dat[1])}
+        
+        # fill keys
+        idx = 2
+        for key in keys:
+            if gshap == True:
+                # convert to m/s**2
+                tmpdict[key] = float(dat[idx]) / g
+            else:
+                tmpdict[key] = float(dat[idx])
+            idx += 1
+        
+        # add to grid list
+        grddict.append(tmpdict)
+        
+    ##############################################################################    
+    # get key index for plotting
+    ##############################################################################
+    
+    for i, key in enumerate(keys):
+        keyProb = str(int(floor(100*float(key.split('-')[-1]))))
+        if keyProb == pltProbability:
+            mapidx = i
+    
+    ##############################################################################    
+    # now make maps
+    ##############################################################################
+    
+    #keys = ['PGA_10', 'PGA_02', 'SA02_10', 'SA02_02', 'SA10_10', 'SA10_02']
+    for i, key in enumerate([keys[mapidx]]): # just plot 1 for now!
+        if i > 0:
+            plt.clf()
+            plt.cla()
+    
+        # get IM period
+        period = key.split('-')[0]
+        period = period.replace('(','')
+        period = period.replace(')','')
+        period = period.replace('.','')
+        
+        # get map probability of exceedance
+        probFraction = str(float(key.split('-')[-1]))
+        probability = str(100*float(key.split('-')[-1])).split('.')[0]+'%'
+        #probability = str(100*float(key.split('-')[-1]))+'%'
+        if probability == '9%':
+            probability = '9.5%'
+        print 'Probability', probability
+        
+        bbox = '108/152/-44/-8' # map boundary - lon1/lon2/lat1/lat2
+        bbox = '107.0/153.0/-43.0/-8.0'
+        
+        bbox = bbox.split('/')
+        minlon = float(bbox[0])
+        maxlon = float(bbox[1])
+        minlat = float(bbox[2])
+        maxlat = float(bbox[3])
+        mbuff = 1.
+        
+        # build data to plot
+        hazvals = []
+        latlist = []
+        lonlist = []
+        
+        # add buffer to data
+        for gridval in grddict:
+            lonlist.append(gridval['lon'])
+            latlist.append(gridval['lat'])
+            if gridval[key] == 0.0:
+                hazvals.append(0.0)
+            else:
+                hazvals.append(gridval[key])
+                
+        #idx = array(range(0, len(lonlist), 10)) # resample for quickly testing mapping
+        idx = array(range(0, len(lonlist), 1))
+        lonlist = array(lonlist)[idx]
+        latlist = array(latlist)[idx]
+        hazvals = array(hazvals)[idx]
+        
+        # delete zero hazvals
+        idx =where(hazvals==0)[0]
+        '''
+        lonlist = delete(lonlist, idx)
+        latlist = delete(latlist, idx)
+        hazvals = delete(hazvals, idx)
+        '''
+        hazvals[idx] = 1E-20
+        
+        # get map bounds
+        llcrnrlat = minlat
+        urcrnrlat = maxlat
+        llcrnrlon = minlon
+        urcrnrlon = maxlon
+        lon_0 = mean([llcrnrlon, urcrnrlon])
+        lon_0 = 134.
+        lat_1 = percentile([llcrnrlat, urcrnrlat], 25)
+        lat_2 = percentile([llcrnrlat, urcrnrlat], 75)
+        
+        # set map
+        # Projection used for National Mapping
+        m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat, \
+                    urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,
+                    projection='lcc',lat_1=lat_1,lat_2=lat_2,lon_0=lon_0,
+                    resolution=res,area_thresh=2000.)
+                    
+        #m.drawmapboundary(fill_color='lightgray')
+        #m.fillcontinents(color='white',lake_color='lightgray',zorder=0)
+        m.drawcoastlines(linewidth=0.5,color='k')
+        m.drawcountries(color='0.2')
+        m.drawstates(color='0.2')
+        
+        # draw parallels and meridians.
+        if maxlon-minlon > 40:
+            xlabel = 6.
+        elif maxlon-minlon > 20:
+            xlabel = 4.
+        elif maxlon-minlon > 10:
+            xlabel = 2.
+        else:
+            xlabel = 1.
+            
+        if maxlat-minlat > 40:
+            ylabel = 6.
+        elif maxlat-minlat > 20:
+            ylabel = 4.
+        elif maxlat-minlat > 10:
+            ylabel = 2.
+        else:
+            ylabel = 1.
+                
+        m.drawparallels(arange(-90.,90.,ylabel), labels=[0,0,0,0],fontsize=10, dashes=[2, 2], color='0.5', linewidth=0.5)
+        m.drawmeridians(arange(0.,360.,xlabel), labels=[0,0,0,0], fontsize=10, dashes=[2, 2], color='0.5', linewidth=0.5)
+        
+        # first make regular cartesian grid
+        print 'Resampling data...'
+        N = 500j
+        extent = (minlon-mbuff, maxlon+mbuff, minlat-mbuff, maxlat+mbuff)
+        xs,ys = mgrid[extent[0]:extent[1]:N, extent[2]:extent[3]:N]
+        	
+        resampled = griddata(lonlist, latlist, log10(hazvals), xs, ys, interp='linear')
+        #resampled = griddata(lonlist, latlist, log10(hazvals), lonlist, latlist, interp='linear') # if this suddenly works, I have no idea why!
+        
+        # get 1D lats and lons for map transform
+        lons = ogrid[extent[0]:extent[1]:N]
+        lats = ogrid[extent[2]:extent[3]:N]
+        
+        # transform to map projection
+        nx = int((m.xmax-m.xmin)/2000.)+1
+        ny = int((m.ymax-m.ymin)/2000.)+1
+        
+        # differences in the way different machines deal with grids - weird!
+        if cwd.startswith('/nas'):
+            transhaz = m.transform_scalar(resampled.T,lons,lats,nx,ny)
+        else:
+            transhaz = m.transform_scalar(resampled.T,lons,lats,nx,ny)
+        
+        masked_array = ma.array(transhaz, mask=isnan(transhaz))
+        #masked_array = masked_array.set_fill_value(0)
+        
+        # get colormap from cpt file
+        cptfile = 'cw1-013_mod.cpt'
+        #ncols = 9
+        
+        #cmap = cm.rainbow
+        #print period
+        if period == 'PGA':
+            
+            if probability == '10%' or probability == '9.5%': # kluge to get on same scale
+                ncolours = 13
+                vmin = -2.
+                vmax = -0.25 - 0.125 # so there is an odd number for which to split the cpt
+                
+            elif probability == '2%':
+                ncolours = 12
+                vmin = -1.5
+                vmax = vmin + 0.25 * ncolours/2.
+            T = 'PGA'
+            
+        elif period == 'SA02':
+            ncolours = 12
+            ncolours = 13
+            if probability == '10%' or probability == '9.5%':
+                vmin = -2
+                vmax = 0.
+                vmax = -0.25 - 0.125 # so there is an odd number for which to split the cpt
+            
+            elif probability == '2%':
+                ncolours = 12
+                vmin = -1.5
+                vmax = vmin + 0.25 * ncolours/2.
+                
+            T = 'Sa(0.2)'
+            
+        elif period == 'SA10':
+            ncolours = 16 
+            ncolours = 13
+            if probability == '10%' or probability == '9.5%':
+                vmin = -2.5
+                vmax = vmin + 0.25 * ncolours/2.
+                vmax = -0.75 - 0.125 # so there is an odd number for which to split the cpt
+            
+            elif probability == '2%':
+                ncolours = 14
+                vmin = -2.25
+                vmax = vmin + 0.25 * ncolours/2.
+                #vmax = -0.25 - 0.125 # so there is an odd number for which to split the cpt
+            T = 'Sa(1.0)'
+        
+        try:
+            cmap, zvals = cpt2colormap(cptfile, ncolours, rev=True)
+        except:
+            try:
+                if gshap == True:
+                    nascptfile = '/nas/gemd/ehp/georisk_earthquake/hazard/DATA/cpt/gshap.cpt'
+                else:
+                    nascptfile = '/nas/gemd/ehp/georisk_earthquake/modelling/sandpits/tallen/NSHA2018/postprocessing/maps/'+ cptfile
+                
+                #cptfile = '/nas/gemd/ehp/georisk_earthquake/modelling/sandpits/tallen/NSHA2018/postprocessing/maps/GMT_no_green.cpt'
+                cmap, zvals = cpt2colormap(nascptfile, ncolours, rev=True)
+            except:
+                try:
+                    ncicptfile = '/short/w84/NSHA18/sandpit/tia547/NSHA2018/postprocessing/maps/'+ cptfile
+                    cmap, zvals = cpt2colormap(ncicptfile, ncolours, rev=True)
+    
+                except:
+                    ncicptfile = '/Users/tallen/Documents/Geoscience_Australia/NSHA2018/postprocessing/maps/'+ cptfile
+                    cmap, zvals = cpt2colormap(ncicptfile, ncolours, rev=True)
+        
+        print 'Making map...'    
+        cmap.set_bad('w', 1.0)
+        m.imshow(masked_array, cmap=cmap, extent=extent, vmin=vmin, vmax=vmax, zorder=0)
+        
+        ##########################################################################################
+        # get land & lake polygons for masking
+        ##########################################################################################
+        # mask non-AU polygons
+        nonmask = [0, 1, 2, 3, 4, 6, 7, 11, 13, 16, 17] # polygon number
+        nonmask = [0, 9, 14, 17, 18, 19] # polygon number
+        landpolys = []
+        for pidx, polygon in enumerate(m.landpolygons):
+            maskPoly = True
+            for nmidx in nonmask:
+                if pidx == nmidx:
+                    maskPoly = False 
+            if maskPoly == True:
+                poly = polygon.get_coords()
+                plt.fill(poly[:,0], poly[:,1], 'w')
+            
+        #mask_outside_polygon(polys[1][::-1], ax=None)
+        polys = get_map_polygons(m)
+        mask_outside_polygons(polys, '0.9', plt)
+        
+        # get lake ploygons
+        polygons = []
+        for polygon in m.lakepolygons:
+            poly = polygon.get_coords()
+            plt.fill(poly[:,0], poly[:,1], '0.9')
+            polygons.append(poly)
+        
+        ##########################################################################################
+        # format main axis
+        ##########################################################################################
+        '''
+        titlestr = ' '.join((modnames[i], T, probability, 'in 50-Year Mean Hazard on AS1170.4 Site Class '))    
+        plt.title(titlestr+'$\mathregular{B_e}$')
+        '''
+        
+        # plt letter
+        xlim = ax.get_xlim()
+        xtxt = xlim[1] * 0.02
+        ylim = ax.get_ylim()
+        ytxt = ylim[1] * 0.96
+        plt.text(xtxt, ytxt, pltlett[ii], fontsize=24, va='top', ha='left')
+        
+        xtxt = xlim[1] * 0.02
+        ylim = ax.get_ylim()
+        ytxt = ylim[1] * 0.02
+        plt.text(xtxt, ytxt, modnames[ii], fontsize=22, va='bottom', ha='left')
+        
+        # get map bbox
+        if i == 0:
+            map_bbox = ax.get_position().extents
+        
+        
+'''
+###########################################################################################
+make colourbar
+###########################################################################################
+'''    
+
+# set colourbar
+plt.gcf().subplots_adjust(bottom=0.1)
+cax = figure.add_axes([0.33,0.12,0.34,0.035]) # setup colorbar axes.
+norm = colors.Normalize(vmin=vmin, vmax=vmax)
+cb = colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, orientation='horizontal')
+
+# set cb labels
+#linticks = array([0.01, 0.03, 0.1, 0.3 ])
+logticks = arange(vmin, vmax+0.25, 0.25)
+cb.set_ticks(logticks)
+labels = [str('%0.3f' % 10**x) for x in logticks]
+cb.set_ticklabels(labels)
+cb.ax.tick_params(labelsize=16)
+
+# set title
+titlestr = ' '.join((T, probability, 'in 50-Year Mean Hazard (g)'))
+cb.set_label(titlestr, fontsize=18)
+'''
+# check to see if maps exists
+if path.isdir('maps') == False:
+    mkdir('maps')
+'''    
+# now save png file
+plt.savefig(path.join('multi_hazard_maps_'+outfile.replace(' ','_')+'.'+probFraction+'.png'), \
+            dpi=300, format='png', bbox_inches='tight')
+
+# save pdf file
+'''
+plt.savefig(path.join('maps', 'hazard_map_'+modelName.replace(' ','_')+'.'+key+'.pdf'), \
+            dpi=300, format='pdf', bbox_inches='tight')
+'''
+#plt.show()
+plt.close('all')
+
+#
