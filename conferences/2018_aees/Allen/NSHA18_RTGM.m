daf672b037c8ea02cde3fede6f44105f81630e4c
treviallen <trevor.allen@ga.gov.au>
2018-09-05 09:06:44 +1000

    updating files for rtgm calculation

--- conferences/2018_aees/Allen/NSHA18_RTGM.m
+++ conferences/2018_aees/Allen/NSHA18_RTGM.m
@@ -3,13 +3,13 @@
 % Parse files
 
 locs02 = 'interp_haz_curves_SA(0.2).csv';
-%locs10 = 'NSHA18Loc_mean_hazcurves_10.csv';
+locs10 = 'interp_haz_curves_SA(1.0).csv';
 
-locsfile = locs02
+locsfile = locs10;
 
 probs = [0.02	0.01375	0.01	0.00445	0.0021	0.001	0.0005	0.000404	0.0002	0.0001];
-[lon lat s1 s2 s3 s4 s5 s6 s7 s8 s9 s10] = ...
-     textread(locsfile,'%f%f%f%f%f%f%f%f%f%f%f%f','delimiter',',','headerlines',4);
+[lon lat s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 places] = ...
+     textread(locsfile,'%f%f%f%f%f%f%f%f%f%f%f%f%s','delimiter',',','headerlines',1);
      
 % concat SAs
 
@@ -21,25 +21,25 @@ RTGMs = [];
 RiskCoeffs = [];
 Iplot = 0;
 HazardCurve.AFEs = probs';
-for i=1:length(loc)
-	if ref(i) == 40 % for testing
+for i=1:length(places)
+	%if ref(i) == 40 % for testing
 		HazardCurve.SAs = SAs(i,:)'	
-		disp(['Calculating RTGM for ',loc(i)]);
+		disp(['Calculating RTGM for ',places{i}]);
 		[ RTGM, RiskCoefficient ] = RTGM_Calculator_Ver131017( HazardCurve, Iplot );
 		RTGMs = [RTGMs RTGM];
 		RiskCoeffs = [RiskCoeffs RiskCoefficient];	
-	end
+	%end
 end
 
 % export RTGM values
-header = 'REF,LON,LAT,RTGM,RISKCOEFF'
+header = 'PLACE,LON,LAT,RTGM,RISKCOEFF';
 
-data = [ref lon lat RTGMs' RiskCoeffs']
+data = [places lon lat RTGMs' RiskCoeffs'];
 
 if locsfile == locs02
-	outfile = 'NSHA18Loc_RTGM_02.csv';
+	outfile = 'NSHA18_RTGM_SA(0.2).csv';
 else
-	outfile = 'NSHA18Loc_RTGM_10.csv';
+	outfile = 'NSHA18_RTGM_SA(1.0).csv';
 end
 dlmwrite(outfile, header, 'delimiter','');
 dlmwrite(outfile, data, 'delimiter',',', '-append');
