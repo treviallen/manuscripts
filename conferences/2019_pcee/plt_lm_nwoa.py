ad9234f8a45b3626101b20b51c3b646fab2ea927
treviallen <trevor.allen@ga.gov.au>
2019-01-24 17:25:09 +1100

    adding plt_lm_nwoa.py

---
+++ conferences/2019_pcee/plt_lm_nwoa.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 17:02:41 2019
+
+@author: u56903
+"""
+
+from obspy.io.xseed import Parser
+from data_fmt_tools import remove_low_sample_data
+from obspy import read
+
+datalessPath = '/nas/active/ops/community_safety/ehp/georisk_earthquake/hazard/Networks/IU/IU.IRIS.dataless'
+iu_parser = Parser(datalessPath)
+
+
+# read file
+mseed = '/nas/active/ops/community_safety/ehp/georisk_earthquake/hazard/Ground_Motion/Australian_Earthquakes/20180916.Lake_Muir/waves/201809160455.NWAO.IU.mseed'
+
+st = read(mseed)
+# remove low sample rate data
+new_st = remove_low_sample_data(st)
+new_st.merge()
+
+tr = new_st[2]
+
+seedid = tr.get_id()
+channel = tr.stats.channel
+start_time = tr.stats.starttime
+
+paz = iu_parser.get_paz(seedid, start_time)
+
+# demean and taper data
+tr = tr.detrend(type='demean')
+tr = tr.taper(0.02, type='hann', max_length=None, side='both')
+tr = tr.simulate(paz_remove=paz)
+
+# convert from m/s to mm/s
+tr.data *= 1000.
+tr.plot(color='r', tick_format='%I:%M %p', starttime=tr.stats.starttime+90,endtime=tr.stats.starttime+160)
+
