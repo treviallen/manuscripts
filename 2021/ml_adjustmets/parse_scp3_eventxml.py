d38f905c2621339429c178bf8403be04f4240cca
treviallen <trevor.allen@ga.gov.au>
2021-01-09 17:28:11 +1100

    moving ml adjustment files

---
+++ 2021/ml_adjustmets/parse_scp3_eventxml.py
@@ -0,0 +1,173 @@
+from misc_tools import dictlist2array, listdir_extension, get_mpl2_colourlist, \
+                       get_binned_stats
+from os import path
+from numpy import nan, nanmedian, arange
+import matplotlib.pyplot as plt
+from obspy import UTCDateTime
+
+import matplotlib as mpl
+mpl.style.use('classic')
+
+def strip_val(line):
+    value = line.strip().split('>')[1].split('</')[0]
+    return float(value)
+
+def strip_val_str(line):
+    return line.strip().split('>')[1].split('</')[0]
+
+#scpxmlfile = 'bulk_sc3xml/ga2019aaxinx_sc3_event_parameters.xml'
+
+# get scp3 xml files
+scpxmlfiles = listdir_extension('bulk_sc3xml', 'xml')
+
+evdict = []
+# loop thru files
+for scpxmlfile in scpxmlfiles:
+    print(scpxmlfile)
+    lines = open(path.join('bulk_sc3xml', scpxmlfile)).readlines()
+    fillParams = True
+    ev = {}
+    
+    # loop thru lines in xml file
+    for i, line in enumerate(lines):
+        # get location
+        if line.strip().startswith('<origin publicID'):
+            ev['origin'] = UTCDateTime(strip_val_str(lines[i+2]))
+            	
+        if line.strip().startswith('<latitude>'):
+            ev['lat'] = strip_val(lines[i+1])
+            	
+        if line.strip().startswith('<longitude>'):
+            ev['lon'] = strip_val(lines[i+1])
+            
+        if line.strip().startswith('<depth>'):
+            ev['dep'] = strip_val(lines[i+1])
+        
+        # get magnitude
+        if line.strip().startswith('<magnitude publicID') and fillParams == True:
+            ev['mag'] = strip_val(lines[i+2])
+            if lines[i+3].strip().startswith('<uncertainty>'):
+                ev['nstas'] = strip_val(lines[i+7])
+                ev['mag_uncert'] = strip_val(lines[i+3])
+                if ev['mag_uncert'] == 0.:
+                    ev['mag_uncert'] = nan
+                ev['mag_type'] = strip_val_str(lines[i+5])
+            else:
+                ev['nstas'] = strip_val(lines[i+6])
+                ev['mag_uncert'] = nan
+                ev['mag_type'] = strip_val_str(lines[i+4])
+            
+            fillParams = False
+    
+    # only add MLa with uncert
+    if 'mag_uncert' in ev.keys():
+        if ev['mag_type'] == 'MLa':
+            evdict.append(ev)
+
+###############################################################################
+# get array and plot hist
+###############################################################################
+fig = plt.figure(1, figsize=(11,11))
+evmag_uncert = dictlist2array(evdict, 'mag_uncert')
+evmag = dictlist2array(evdict, 'mag')
+origin = dictlist2array(evdict, 'origin')
+allmags = evmag
+alluncert = evmag_uncert
+print(min(origin))
+print(max(origin))
+print('median =', nanmedian(evmag_uncert))
+
+ax = plt.subplot(221)
+plt.hist(evmag_uncert, bins=arange(0.0, 1.0, 0.05), facecolor='0.7')
+
+#plt.xlabel('MLa Uncertainty', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.xlim([0, 1.])
+
+# add label
+props = dict(boxstyle='round', facecolor='w', alpha=1)
+text = ' '.join((r'$\tilde{x}_{All}$', '=', str('%0.2f' % nanmedian(evmag_uncert))+'\n',\
+                 r'$\it{n}$', '=', str(len(evmag_uncert)),''))
+ylims = ax.get_ylim()
+plt.text(0.95, ylims[1]*0.95, text, ha='right', va='top', fontsize=16, bbox=props)
+plt.text(0.02*0.9, ylims[1]*0.98, '(a)', ha='left', va='top', fontsize=16)
+
+###############################################################################
+# get array and plot hist
+###############################################################################
+
+import shapefile
+from shapely.geometry import Point, Polygon
+from mapping_tools import get_field_data
+
+shpfile = 'shapefile/australia_ml_regions.shp'
+sf = shapefile.Reader(shpfile)
+shapes = sf.shapes()
+polygons = []
+for poly in shapes:
+    polygons.append(Polygon(poly.points))
+    
+ml_reg = get_field_data(sf, 'ML_REGION', 'str')
+mmin = 2.45
+
+i = 2
+letter = ['(b)', '(c)', '(d)']
+reg_dict = []
+for poly, reg in zip(polygons, ml_reg):
+    reg_evdict = []
+    for ev in evdict:
+        pt = Point(ev['lon'], ev['lat'])
+        if pt.within(poly) and ev['mag'] >= mmin:
+            reg_evdict.append(ev)
+            
+    # now plot regions
+    evmag_uncert = dictlist2array(reg_evdict, 'mag_uncert')
+    evmag = dictlist2array(reg_evdict, 'mag')
+    
+    print('median =', nanmedian(evmag_uncert))
+    
+    ax = plt.subplot(2,2,i)
+    plt.hist(evmag_uncert, bins=arange(0.0, 1.0, 0.05), facecolor='0.7')
+    
+    if i > 2:
+        plt.xlabel('$\mathregular{M_L}$ Uncertainty', fontsize=16)
+    if i == 3:
+        plt.ylabel('Count', fontsize=16)
+    plt.xlim([0, 1.])
+    
+    text = ' '.join((r'$\tilde{x}_{'+reg+'}$', '=', str('%0.2f' % nanmedian(evmag_uncert))+'\n',\
+                     r'$\it{n}$', '=', str(len(evmag_uncert)),''))
+    ylims = ax.get_ylim()
+    plt.text(0.95, ylims[1]*0.95, text, ha='right', va='top', fontsize=16, bbox=props)
+    plt.text(0.02*0.9, ylims[1]*0.98, letter[i-2], ha='left', va='top', fontsize=20)
+   
+    i += 1
+    
+    reg_tmp = {'mag_uncert': evmag_uncert, 'mag': evmag, 'reg': reg}
+    print (len(evmag_uncert))
+    print (len(evmag))
+    reg_dict.append(reg_tmp)
+
+plt.savefig('ml_uncertainty_histograms.png', fmt='png', bbox_inches='tight')
+plt.show()
+
+###############################################################################
+# plot mag uncert with mag
+###############################################################################
+
+fig = plt.figure(2, figsize=(10,5))
+sym = ['^', 'D', 'o']
+cols = get_mpl2_colourlist()
+for i, reg in enumerate(reg_dict):
+    plt.plot(reg['mag'], reg['mag_uncert'], sym[i], mec=cols[i], mfc='none', mew=1.5, label = reg['reg'])
+
+# get binned stats
+bins = arange(2.5, 6.1, 0.1)
+med_uncert, std_uncert, medx, bins, nperbin = get_binned_stats(bins, allmags, alluncert)
+plt.errorbar(bins, med_uncert, yerr=std_uncert, fmt='rs', label='Binned Median')  
+plt.legend(loc=1, numpoints=1)
+plt.ylim([0, 1])
+plt.xlim([2.4, 5])
+
+plt.savefig('ml_uncertainty_vs_ml.png', fmt='png', bbox_inches='tight')    
+plt.show()
---
+++ 2021/ml_adjustmets/plt_mlpref_vs_mlrev.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.style.use('classic')
+from numpy import loadtxt, arange, array
+from scipy.stats import linregress
+from misc_tools import get_binned_stats
+from scipy.odr import RealData, Model, ODR, models
+
+magdiffcsv = '/Users/trev/Documents/Geoscience_Australia/NSHA2018/catalogue/matlab/rev_mag_diff.csv'
+
+data = loadtxt(magdiffcsv, delimiter=',')
+
+fig = plt.figure(1, figsize=(7,7))
+
+plt.plot([2,7], [2,7], '--', c='0.25', lw=2.0, label='1:1')
+plt.plot(data[:,0], data[:,1], 'r+', label='Data')
+	
+# bin data and regress
+mbins = arange(1, 6, 0.2)
+medres, stdres, medx, mbins, nperbin = get_binned_stats(mbins, data[:,0], data[:,1])
+reg_pars = linregress(mbins, medres)
+xplt = array([2, 7])
+yplt = reg_pars[0] * xplt + reg_pars[1]
+#plt.plot(xplt, yplt, 'k-', label='LSQ')
+
+reg_pars2 = linregress(data[:,0], data[:,1])
+
+# now do ODR
+# Define a function (quadratic in our case) to fit the data with.
+def linear_func(p, x):
+   m, c = p
+   return m*x + c
+
+# Create a model for fitting.
+linear_model = Model(linear_func)
+# Create a RealData object using our initiated data from above.
+data = RealData(data[:,0], data[:,1])
+# Set up ODR with the model and data.
+odr = ODR(data, linear_model, beta0=[1., 0.])
+odr.set_job(fit_type=0) #if set fit_type=2, returns the same as leastsq
+# Run the regression.
+out = odr.run()
+# Use the in-built pprint method to give us results.
+out.pprint()
+modr = out.beta[0]
+codr = out.beta[1]
+yplt_odr = modr * xplt + codr
+plt.plot(xplt, yplt_odr, 'b-', lw=2, label='ODR')
+
+
+plt.legend(numpoints=1, loc=2)
+plt.grid(which='both')
+plt.xlabel('Original $\mathregular{M_L}$ $\mathregular{(M_{LH})}$', fontsize=16)
+plt.ylabel('Revised $\mathregular{M_L}$ $\mathregular{(M_{LR})}$', fontsize=16)
+
+plt.savefig('hist_mag_fit.png', fmt='png', bbox_inches='tight')
+
+plt.show()
