e970280f5066163943eea5f66c15ced0983527a7
treviallen <trevor.allen@ga.gov.au>
2021-02-23 16:58:26 +1100

    adding domains to scenario map

--- 2021/shakemap_scenarios/map_scenario_locations.py
+++ 2021/shakemap_scenarios/map_scenario_locations.py
@@ -6,8 +6,10 @@ from mpl_toolkits.basemap import Basemap
 from numpy import arange, mean, percentile, array, unique, where, argsort, floor , loadtxt
 from netCDF4 import Dataset as NetCDFFile
 from gmt_tools import cpt2colormap
+from mapping_tools import drawshapepoly
 from os import path, walk, system
 from mapping_tools import labelCentroid
+import shapefile
 
 plt.rcParams['pdf.fonttype'] = 42
 mpl.style.use('classic')
@@ -47,7 +49,7 @@ m = Basemap(projection='lcc',lat_1=lat_1,lat_2=lat_2,lon_0=lon_0,\
 # draw coastlines, state and country boundaries, edge of map.
 m.shadedrelief()
 #m.drawcoastlines()
-#m.drawstates()
+m.drawstates(linewidth=0.5, color='0.3')
 
 m.drawparallels(arange(-90.,90.,6.), labels=[1,0,0,0],fontsize=16, dashes=[2, 2], color='0.5', linewidth=0.75)
 m.drawmeridians(arange(0.,360.,10.), labels=[0,0,0,1], fontsize=16, dashes=[2, 2], color='0.5', linewidth=0.75)
@@ -62,14 +64,20 @@ data = loadtxt('scenario_lat_lons.csv', delimiter=',')
 x, y = m(data[:,1], data[:,0])
 plt.plot(x, y, '*', mfc='r', mec='k', mew=0.25, markersize=8, label='Scenario Localities')
 
+'''
 path_effect=[path_effects.withStroke(linewidth=3, foreground='w')]
 x, y = m(data[:,0]+0.75, data[:,1]+0.5)
 for i in range(0, len(x)):
     plt.text(x[i], y[i], 'E'+str(i+1), fontsize=14, c='b', \
              va='bottom', ha='left', style='italic', path_effects=path_effect)
-    
+'''    
 plt.legend(loc=3, fontsize=14, numpoints=1)
 
-plt.savefig('scenario_locations.png', format='png', bbox_inches='tight')
+# add neotectonic domains
+shpfile = '/nas/active/ops/community_safety/ehp/georisk_earthquake/modelling/sandpits/tallen/NSHA2018/source_models/zones/shapefiles/Domains/Domains_Sep2011.shp'
+sf = shapefile.Reader(shpfile)
+drawshapepoly(m, plt, sf, col='k', lw=.5)
+
+plt.savefig('scenario_locations.png', format='png', dpi=300, bbox_inches='tight')
 #plt.savefig('neac_ml_zones.svg', format='svg', bbox_inches='tight')
 plt.show()
