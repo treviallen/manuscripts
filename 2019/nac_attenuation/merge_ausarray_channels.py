9b8b584151672c49ee428f796540a4dc11930570
treviallen <trevor.allen@ga.gov.au>
2021-07-21 21:50:56 +1000

    files for handling mseed data

---
+++ 2019/nac_attenuation/merge_ausarray_channels.py
@@ -0,0 +1,30 @@
+from obspy import read
+from misc_tools import listdir_extension
+from sys import argv
+from os import path
+
+folder = argv[1]
+
+files = listdir_extension(folder, 'HHE')
+
+for f in files:
+	print(f)
+	try:
+		st = read(path.join(folder, f))
+		
+		stn = read(path.join(folder, f.replace('HHE','HHN')))
+		st += stn
+		
+		stz = read(path.join(folder, f.replace('HHE','HHZ')))
+		st += stz
+		
+		# write to file
+		tr = st[0]
+		mseed = path.join('mseed', \
+                           '.'.join((tr.stats.starttime.strftime('%Y-%m-%dT%H.%M'), \
+                           tr.stats['network'], tr.stats['station'], 'mseed')))
+		#mseed = path.join('mseed', f.replace('HHE','mseed'))
+		st.write(mseed, format="MSEED")
+		
+	except:
+		print('  Cannot read file')
