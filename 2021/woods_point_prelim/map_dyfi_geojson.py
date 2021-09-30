bf2e1aa6a83cfe4ee48718f16ec80f7eb57c3770
treviallen <trevor.allen@ga.gov.au>
2021-09-30 10:00:23 +1000

    initial wp eq commit

---
+++ 2021/woods_point_prelim/map_dyfi_geojson.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 16:32:05 2019
+
+@author: u56903
+"""
+from mapping_tools import make_street_map, get_map_polygons, mask_outside_polygons, annotate_cities
+from numpy import mean, percentile, arange
+from mpl_toolkits.basemap import Basemap
+from matplotlib import colors, colorbar
+from gmt_tools import cpt2colormap, remove_last_cmap_colour
+import matplotlib.pyplot as plt
+from os import getcwd
+from sys import argv
+import matplotlib as mpl
+mpl.style.use('classic')
+
+import json
+from numpy import array
+
+#jsonFilePath = 'aggregation/canberra_1_1.geojson'
+#jsonFilePath = 'events/20190714.Broome/felt_reports_5km.geojson'
+
+jsonFilePath = argv[1]
+
+with open(jsonFilePath) as f:
+    data = json.load(f)
+
+dyfi_dict = []
+for feature in data['features']:
+    tmp = {'geomerty':feature['geometry']['coordinates'][0],
+           'centroid':feature['properties']['center']['coordinates'],
+           'intensity':feature['properties']['intensityFine'],
+           'nresp':feature['properties']['nresp']}
+    
+    # append to list
+    dyfi_dict.append(tmp)
+        
+###############################################################################
+# set eq details
+###############################################################################
+
+# Woods Point
+mag  = 5.9
+eqdep = 12.0
+eqla = -37.5063
+eqlo = 146.4022
+degrng = 5.9
+latoff = -0.0
+lonoff = -1.2
+mstr = '5.9'
+place = 'Woods Point, VIC'
+evid = '2021-09-21'
+
+
+##########################################################################################
+# set up map
+##########################################################################################
+'''
+plt, m = make_street_map(eqla, eqlo, service='ESRI_Imagery_World_2D', ll_buffer = degrng, \
+             xpixels = 1500, plt_inset = True, inset_state = 'nsw', inset_loc = 3, \
+             plt_marker = True, marker='*', ms = 14, mew = 1., mfc = 'none', mec='r')
+'''
+
+##########################################################################################
+# set up map
+##########################################################################################
+
+# set map on fly
+if degrng < 0.5:
+    parSpace = 0.2
+    merSpace = 0.2
+    scaleLength = 15
+    res = 'f'
+elif degrng < 1:
+    parSpace = 0.4
+    merSpace = 0.4
+    scaleLength = 30
+    res = 'f'
+elif degrng >= 1 and degrng <= 2:
+    parSpace = 1.
+    merSpace = 1.
+    res = 'h'
+    scaleLength = 50
+elif degrng > 2 and degrng < 5:
+    parSpace = 1.
+    merSpace = 2.
+    res = 'h'
+    scaleLength = 100
+elif degrng >= 5.:
+    parSpace = 2.
+    merSpace = 2.
+    res = 'i'
+    scaleLength = 400
+    
+# make bounds on the fly - adjust to make square-ish
+urcrnrlat = eqla + degrng + latoff
+llcrnrlat = eqla - degrng + latoff
+urcrnrlon = eqlo + degrng*1.2 + lonoff
+llcrnrlon = eqlo - degrng*1.2 + lonoff
+
+# set up figure
+fig = plt.figure(figsize=(18,10))
+plt.tick_params(labelsize=16)
+ax = fig.add_subplot(111)
+
+# set projection
+lon_0 = mean([llcrnrlon, urcrnrlon])
+lat_1 = percentile([llcrnrlat, urcrnrlat], 25)
+lat_2 = percentile([llcrnrlat, urcrnrlat], 75)
+
+m = Basemap(projection='merc',\
+            llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat, \
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,\
+            rsphere=6371200.,resolution=res,area_thresh=200)
+
+# draw coastlines, state and country boundaries, edge of map.
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+
+m.fillcontinents(color='0.9',lake_color='lightskyblue')
+m.drawparallels(arange(-90.,90.,parSpace), labels=[1,0,0,0],fontsize=16, dashes=[2, 2], color='0.99', linewidth=0.0)
+m.drawmeridians(arange(0.,360.,merSpace), labels=[0,0,0,1], fontsize=16, dashes=[2, 2], color='0.99', linewidth=0.0)
+
+# get scale position and length
+slon = m.llcrnrlon + 0.15*(m.urcrnrlon - m.llcrnrlon)
+slat = m.llcrnrlat + 0.06*(m.urcrnrlat - m.llcrnrlat)
+slon0 = mean([m.urcrnrlon, m.llcrnrlon])
+slat0 = mean([m.urcrnrlat, m.llcrnrlat])
+
+m.drawmapscale(slon, slat, slon0, slat0, scaleLength, fontsize = 14, barstyle='fancy', zorder=1000)
+
+##########################################################################################
+# get land & lake polygons for masking
+##########################################################################################
+
+polys = get_map_polygons(m)
+
+mask_outside_polygons(polys, 'lightskyblue', plt)
+
+# get lake ploygons and mask
+polygons = []
+for polygon in m.lakepolygons:
+    poly = polygon.get_coords()
+    plt.fill(poly[:,0], poly[:,1], 'lightskyblue')
+    polygons.append(poly)
+
+##########################################################################################
+# get cmap
+##########################################################################################
+# get colormap
+if getcwd().startswith('/nas'):
+    cptfile = '/nas/active/ops/community_safety/ehp/georisk_earthquake/hazard/DATA/cpt/mi_pop.cpt'
+else:
+    cptfile = '//Users//trev//Documents//DATA//GMT//cpt//mi_pop.cpt'
+ncols = 10
+cmap, zvals = cpt2colormap(cptfile, ncols+1, rev=False)
+cmap = remove_last_cmap_colour(cmap)
+cs = (cmap(arange(ncols)))
+
+##########################################################################################
+# plt dyfi
+##########################################################################################
+
+# loop thru grid cells values and plt MMI
+nresp = 0
+min_resp = 0
+for dyfi in dyfi_dict:        
+    # add to list greater than minObs
+    if dyfi['nresp'] > min_resp:
+        
+        # now plot
+        pltx = array(dyfi['geomerty'])[:,0]
+        plty = array(dyfi['geomerty'])[:,1]
+        
+        x, y = m(pltx, plty)
+        colidx = int(round(dyfi['intensity']))-1
+        c= tuple(cs[colidx][:-1])
+        plt.fill(x, y, fc=c, ec='0.45', lw=0.25, zorder=100)
+        
+    nresp += dyfi['nresp']
+        
+##########################################################################################
+# annotate
+##########################################################################################
+
+# plt earthquake epicentre
+x, y = m(eqlo, eqla)
+plt.plot(x, y, '*', markersize=25, markerfacecolor='None', mec='r', mew=1.5, zorder=1000000)
+
+# annotate cities
+numCities = 21
+annotate_cities(numCities, plt, m, markersize=5)
+
+# add n responses txt
+lonrng = m.urcrnrlon - m.llcrnrlon
+latrng = m.urcrnrlat - m.llcrnrlat
+plttxt = 'Number of Responses = '+str(nresp)
+x, y = m(m.llcrnrlon+0.02*lonrng, m.llcrnrlat+0.98*latrng)
+
+props = dict(boxstyle='round', facecolor='w', alpha=1)
+plt.text(x, y, plttxt, size=16, ha='left', va='top', bbox=props, zorder=1000000)
+
+##########################################################################################
+# make colorbar
+##########################################################################################
+
+# set colourbar
+plt.gcf().subplots_adjust(bottom=0.13)
+cax = fig.add_axes([0.34,0.05,0.33,0.03]) # setup colorbar axes.
+
+norm = mpl.colors.Normalize(vmin=0.5, vmax=10.5)#myb
+cb = colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, orientation='horizontal')
+
+# set cb labels
+ticks = range(1,11)
+rom_num = ['I', 'II', 'III', 'IV', 'V', 'VI','VII','VIII','IX','X']
+cb.set_ticks(ticks)
+cb.set_ticklabels(rom_num)
+
+titlestr = 'Macroseismic Intensity'
+cb.set_label(titlestr, fontsize=16)
+
+plt.savefig('_'.join((evid.replace('-',''), place.split(',')[0].replace(' ', '_'), \
+            'gridded_mmi_data_gridded.png')), format='png', bbox_inches='tight', dpi=300)
+plt.show()
---
+++ 2021/woods_point_prelim/wp_mmi_atten.py
@@ -0,0 +1,199 @@
+#moe_mmi_atten.py
+from mmi_tools import allen_etal_2012_rrup_ipe, allen_etal_2012_rhypo_ipe, \
+                      atkinson_wald_ceus_ipe, atkinson_wald_cal_ipe, pgm2mmi_worden12, \
+                      parse_usgs_dyfi_geocoded, parse_usgs_dyfi_zip, \
+                      atkinson_worden_wald14_cal_ipe, atkinson_worden_wald14_ceus_ipe, \
+                      atkinson_worden_wald14_ceus_oq
+from misc_tools import get_mpl2_colourlist
+from calc_oq_gmpes import scr_gsims, crustal_gsims
+from mapping_tools import distance
+from shakemap_tools import write_mmi_obs_raw
+from misc_tools import get_binned_stats_mean
+from numpy import array, arange, log10, exp, logspace, sqrt, isnan, nan, hstack
+from scipy.special import erf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import json
+mpl.style.use('classic')
+
+plt.rcParams['pdf.fonttype'] = 42
+
+fig = plt.figure(figsize=(10, 6))
+plt.tick_params(labelsize=12)
+
+# event details
+mag = 5.9
+eqdep = 12 
+eqlat = -37.5063
+eqlon = 146.4022
+ztor = 4.
+maxrrup = 510.
+rjb = logspace(0, log10(maxrrup), 60)
+rrup = sqrt(rjb**2 + eqdep**2)
+rhypo = rrup
+repi = rjb
+rake = 0. # USGS CMT
+dip  = 90.
+vs30 = 360.
+
+####################################################################################
+# now read and plot DYFI data
+dyfifile = 'felt_reports/felt_reports_1km.geojson'
+
+with open(dyfifile) as f:
+    data = json.load(f)
+
+dyfimmi  = []
+dyfilat = []
+dyfilon = []
+for feature in data['features']:
+    rec = {'geomerty':feature['geometry']['coordinates'][0],
+           'centroid':feature['properties']['center']['coordinates'],
+           'intensity':feature['properties']['intensityFine'],
+           'nresp':feature['properties']['nresp']}
+    
+    if rec['nresp'] >= 2:
+       dyfimmi.append(rec['intensity'])
+       dyfilat.append(rec['centroid'][1])
+       dyfilon.append(rec['centroid'][0])
+       
+# calc DYFI hypo dist
+dyfilat = array(dyfilat)
+dyfilon = array(dyfilon)
+dyfirepi = []
+for i in range(0, len(dyfilat)):
+    dyfirepi.append(distance(eqlat, eqlon, dyfilat[i], dyfilon[i])[0])
+dyfirhyp = sqrt(array(dyfirepi)**2 + eqdep**2)
+
+# now plot
+d3 = plt.semilogx(dyfirepi, dyfimmi, '+', lw=2., color='0.6', ms=6)
+#d3 = plt.semilogx(dyfirepi[0], dyfimmi[0], '+', lw=2., color='0.6', ms=6)
+
+# write DYFI MMI4SM
+#write_mmi_obs_raw('DYFI', dyfidict)
+
+####################################################################################
+# parse AU ipe
+####################################################################################
+lines = open('../au_ipe/au_ipe_coefs.dat').readlines()
+
+vert = 7.
+c0 = float(lines[1].strip().split()[-1]) 
+c1 = float(lines[2].strip().split()[-1]) 
+c2 = float(lines[3].strip().split()[-1]) 
+c3 = float(lines[4].strip().split()[-1]) 
+rref = float(lines[5].strip().split()[-1]) 
+xh = float(lines[6].strip().split()[-1]) 
+h1 = float(lines[7].strip().split()[-1]) 
+h2 = float(lines[8].strip().split()[-1]) 
+h3 = float(lines[9].strip().split()[-1]) 
+
+# Allen 2019 deep
+A19_deep = c0 * mag + c1 + c2 * log10(sqrt(rrup**2 + rref**2)) + (h1*erf((eqdep-vert)/(h2*sqrt(2))) + h3)
+
+####################################################################################
+# plot models   
+
+# do AW07 CEUS
+AW07ceus, sig = atkinson_wald_ceus_ipe(mag, rrup)
+
+# do biased AW07 CEUS
+#AW07ceus_bias, sig = atkinson_wald_ceus_ipe(mag+SMbias, rrup)
+
+# do AW07 CA
+AW07cal, sig = atkinson_wald_cal_ipe(mag, rrup)
+
+# do Allen et al 2012 Rrup
+Aea12rup, Aea12sig = allen_etal_2012_rrup_ipe(mag, rrup, eqdep)
+
+# do Allen et al 2012 Rhypo
+Aea12hypo, Aea12sig = allen_etal_2012_rhypo_ipe(mag, rrup, eqdep)
+
+# do AWW14 CEUS
+SMbias = -0.233
+AWW14ceus, sig = atkinson_worden_wald14_ceus_ipe(mag, rhypo, repi)
+AWW14ceus_bias = AWW14ceus + SMbias # from Moe
+
+# do AWW14 CA
+AWW14cal, sig = atkinson_worden_wald14_cal_ipe(mag, rhypo)
+
+#AWW14ceus_oq, sig_oq = atkinson_worden_wald14_ceus_oq(mag, rhypo, eqdep) # this works, but not needed
+
+# do Bea14 - W12 PGA
+logB14pga = [] # tried pgv, but looked really bad!
+
+for i in range(0, len(rjb)):
+    #Bea97imt, Zea06imt, CB08imt, CY08imt, Bea11imt, BA11imt, AA13imt, Aea14imt, Bea14imt, CB14imt, CY14imt \
+    #= crustal_gsims(mag, eqdep, ztor, dip, rake, rrup[i], rjb[i], vs30)
+    Tea02imt, C03imt, AB06imt, CY08imt, Sea09imt, Pea11imt, A12imt, Bea14imt, YA15imt \
+    = scr_gsims(mag, eqdep, ztor, dip, rake, rrup[i], rjb[i], vs30)
+    logB14pga.append(Bea14imt['pga'][0][0]) 
+
+# convert ln g to cm/s**2
+from scipy.constants import g
+B14pga = exp(logB14pga) * 100. * g
+# now get MMI from GMICE
+#B14W12mmi, sig = pgm2mmi_worden12(10**array(logB14pgv), 'pga', mag, rrup)  
+B14W12mmiPGA, sig = pgm2mmi_worden12(B14pga, 'pga', mag, rrup) 
+
+# do Bea14 - W12 PGV
+logB14pgv = [] # tried pgv, but looked really bad!
+for i in range(0, len(rjb)):
+    #Bea97imt, Zea06imt, CB08imt, CY08imt, Bea11imt, BA11imt, AA13imt, Aea14imt, Bea14imt, CB14imt, CY14imt \
+    #= crustal_gsims(mag, eqdep, ztor, dip, rake, rrup[i], rjb[i], vs30)
+    Tea02imt, C03imt, AB06imt, CY08imt, Sea09imt, Pea11imt, A12imt, Bea14imt, YA15imt \
+    = scr_gsims(mag, eqdep, ztor, dip, rake, rrup[i], rjb[i], vs30)
+    if isnan(Bea14imt['pgv']):
+        logB14pgv.append(nan) 
+    else:
+        logB14pgv.append(Bea14imt['pgv'][0][0]) 
+
+B14pgv = exp(array(logB14pgv))
+# now get MMI from GMICE
+B14W12mmiPGV, sig = pgm2mmi_worden12(B14pgv, 'pgv', mag, rrup)  
+B14W12mmiPGVnan, sig = pgm2mmi_worden12(B14pgv, 'pgv', nan, nan)
+ 
+# now plot models
+#SMbias = -0.16
+cl = get_mpl2_colourlist()
+syms = ['o', '^', 's', 'd', 'v', '<', 'h', '>', 'p']
+
+# make secondary plots to get around color issues
+h1 = plt.semilogx(rjb, AW07ceus, syms[0], color=cl[0], ls='-', ms=7, mec=cl[0], markevery=5)
+h2 = plt.semilogx(rjb, AW07cal,  syms[1], color=cl[1], ls='-', ms=7, mec=cl[1], markevery=5)
+h3 = plt.semilogx(rjb, Aea12rup, syms[2], color=cl[2], ls='-', ms=7, mec=cl[2], markevery=5)
+h5 = plt.semilogx(rjb, AWW14ceus,syms[3], color=cl[3], ls='-', ms=7, mec=cl[3], markevery=5)
+h6 = plt.semilogx(rjb, AWW14cal, syms[4], color=cl[4], ls='-', ms=7, mec=cl[4], markevery=5)
+h7 = plt.semilogx(rjb, A19_deep, syms[5], color=cl[5], ls='-', ms=7, mec=cl[5], markevery=5)
+
+#h4 = plt.semilogx(rjb, B14W12mmiPGA, '-s', color=cl[3], lw=1.5, ms=7, mec=cl[3], markevery=5)
+
+##################################################################################
+
+# get binned stats
+bins = arange(0.05, log10(maxrrup)+0.05, 0.1)
+meanres, stdres, medx, outbins, nperbin = get_binned_stats_mean(bins, log10(dyfirepi), dyfimmi)
+
+d4 = plt.errorbar(10**medx, meanres, yerr=stdres, fmt='ks', ms=7, elinewidth=1.5, capsize=5, zorder=30000) 
+
+leg1 = plt.legend([h1[0], h2[0], h3[0], h5[0], h6[0], h7[0], d3[0], d4[0]], \
+           ['AW07 CEUS', 'AW07 CA', 'Aea12 Rrup', 
+            'AWW14 CEUS', 'AWW14 CA', 'A21 AU', 'Raw DYFI', 'Binned MMI'], fontsize=12, loc=3, numpoints=1)
+
+plt.grid(which='both', color='0.5')
+plt.xlim([8, maxrrup])
+plt.ylim([1, 8])
+plt.xlabel('Epicentral Distance (km)', fontsize=14)
+plt.ylabel('Macroseismic Intensity', fontsize=14)
+
+xtic = [10, 20, 50, 100, 200]
+xlab = ['10', '20', '50', '100', '200']
+plt.xticks(xtic, xlab)
+ylab = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII']
+ytic = range(1, 9)
+plt.yticks(ytic, ylab)
+
+plt.savefig('wp_mmi_atten.png', format='png', dpi=300, bbox_inches='tight')
+#plt.savefig('figures/moe_mmi_atten.svg', format='svg', dpi=300, bbox_inches='tight')
+
+plt.show()
