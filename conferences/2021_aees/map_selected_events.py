6c048fcb73b53a92cc37d9332d9d912587ba6861
treviallen <trevor.allen@ga.gov.au>
2021-11-23 14:26:10 +1100

    adding map_selected_events.py

---
+++ conferences/2021_aees/map_selected_events.py
@@ -0,0 +1,90 @@
+#from matplotlib.mlab import griddata
+from matplotlib import colors, colorbar
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits.basemap import Basemap
+from numpy import arange, mean, percentile, array, unique, where, argsort, floor , loadtxt
+from netCDF4 import Dataset as NetCDFFile
+#from gmt_tools import cpt2colormap
+from os import path, walk, system
+from mapping_tools import labelCentroid
+
+plt.rcParams['pdf.fonttype'] = 42
+mpl.style.use('classic')
+
+##########################################################################################
+#108/152/-44/-8
+urcrnrlat = -5.
+llcrnrlat = -49.
+urcrnrlon = 157.
+llcrnrlon = 98.
+lon_0 = mean([llcrnrlon, urcrnrlon])
+lat_1 = percentile([llcrnrlat, urcrnrlat], 25)
+lat_2 = percentile([llcrnrlat, urcrnrlat], 75)
+
+fig = plt.figure(figsize=(13,8))
+plt.tick_params(labelsize=14)
+ax = fig.add_subplot(111)
+'''
+epsg = 3112
+service='Ocean_Basemap'
+#$service='World_Imagery'
+#service='World_Shaded_Relief'
+m = Basemap(projection='mill',llcrnrlon=llcrnrlon ,llcrnrlat=llcrnrlat,
+            urcrnrlon=urcrnrlon ,urcrnrlat=urcrnrlat, resolution = 'l', epsg = epsg)
+    
+# xpixels controls the pixels in x direction, and if you leave ypixels
+# None, it will choose ypixels based on the aspect ratio
+xpixels = 1500
+m.arcgisimage(service=service, xpixels = xpixels, verbose= True, dpi=300)
+'''
+
+m = Basemap(projection='lcc',lat_1=lat_1,lat_2=lat_2,lon_0=lon_0,\
+            llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat, \
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,\
+            rsphere=6371200.,resolution='l',area_thresh=2000.)
+
+# draw coastlines, state and country boundaries, edge of map.
+m.shadedrelief()
+#m.drawcoastlines()
+#m.drawstates()
+
+m.drawparallels(arange(-90.,90.,6.), labels=[1,0,0,0],fontsize=16, dashes=[2, 2], color='0.5', linewidth=0.75)
+m.drawmeridians(arange(0.,360.,10.), labels=[0,0,0,1], fontsize=16, dashes=[2, 2], color='0.5', linewidth=0.75)
+#m.drawmapscale(144, -34.8, 146., -38.5, 400, fontsize = 16, barstyle='fancy', zorder=100)
+
+##########################################################################################
+# add ficticious earthquakes
+##########################################################################################
+
+import matplotlib.patheffects as path_effects
+csvfile = 'shakemap_event_list.csv'
+data = loadtxt(csvfile, delimiter=',', skiprows=1)
+
+lines = open(csvfile).readlines()[1:]
+lats = []
+lons = []
+for line in lines:
+    dat = line.strip().split(',')
+    lons.append(float(dat[1]))
+    lats.append(float(dat[2]))
+    mags.append(float(dat[4]))
+    
+x, y = m(array(lons), array(lats))
+msize = 4 * mags
+sc = plt.scatter(x, y, m='*', s=msize, mfc='r', mec='k', mew=0.25)
+
+# make legend
+legmag = [3., 5., 7.]
+legh = []
+for lm in legmag:
+    x, y = m(0, 0)
+    h = m.plot(x, y, '*', mfc='r', mec='k', mew=0.25, markersize=(4 * lm), alpha=1., zorder=len(mags)+1)
+    legh.append(h[0])
+
+l = plt.legend(legh, ('MW 3.0', 'MW 5.0', 'MW 7.0'), loc=3, numpoints=1, fontsize=14, title="Magnitude")
+l.set_zorder(len(mags)+5)
+
+plt.savefig('atlas_event_map.png', format='png', bbox_inches='tight')
+#plt.savefig('neac_ml_zones.eps', format='eps', bbox_inches='tight')
+plt.show()
