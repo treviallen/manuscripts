b9a4799c65a88fa6f249467322cd2e72676543fa
treviallen <trevor.allen@ga.gov.au>
2022-04-26 21:22:36 +1000

    adding nge-e and other fixes

--- 2022/woods_point_chimney/get_gmm_pga.py
+++ 2022/woods_point_chimney/get_gmm_pga.py
@@ -29,25 +29,26 @@ rake = 0. # GA CMT
 dip  = 83.
 mag = 5.9
 dep = 10.
-vs30 = 400.
+vs30 = 560.
 
 # parse chimney file
 csvfile = 'Chimney_RJB_GMPE.csv'
 data = loadtxt(csvfile, skiprows=1, delimiter=',')
 rjbs = data[:,1]
+rhyps = data[:,2] # now added
 n_chim = data[:,0]
 
-txt = '#CHIMNEY,RJB,Gea90SEA,Gea90WA,AB06,Sea09NC,Sea09YC,A12,Bea14,Tea19,NGA-E\n'
+txt = '#CHIMNEY,RHYP,RJB,Gea90SEA,Gea90WA,AB06,Sea09NC,Sea09YC,A12,Bea14,NGA-E\n'
 
-for chim, rjb in zip(n_chim, rjbs):
+for chim, rjb, rhyp in zip(n_chim, rjbs, rhyps):
     print(chim)
     rrup = sqrt(rjb**2 + ztor**2)
-    rhyp = rrup = sqrt(rjb**2 + dep**2) # assume for now as need site locations
-
+    #rhyp = rrup = sqrt(rjb**2 + dep**2) # assume for now as need site locations
+    
     Tea02imt, C03imt, AB06imt, AB11imt, Sea09imt_SS14, Sea09YCimt_SS14, Pea11imt, A12imt, A12imt_SS14, Bea14imt \
              = scr_gsims(mag, dep, ztor, dip, rake, rrup, rjb, vs30)
     
-    Tea19 = tang2019_cam_gsim(mag, dep, rrup, vs30)
+    #Tea19 = tang2019_cam_gsim(mag, dep, rrup, vs30)
     
     G90WAimt, G90SEAimt, G90INDimt, G90WA_PGVimt, G90SEA_PGVimt, G90IND_PGVimt = gaull1990_gsim(mag, dep, rhyp)
     
@@ -55,7 +56,8 @@ for chim, rjb in zip(n_chim, rjbs):
     #A12imt = adjust_gmm_with_SS14(A12imt, 820., vs30) - corrected in gsim code
     #Sea09imt = adjust_gmm_with_SS14(Sea09imt, 865., vs30)
     
-    '''
+    
+    
     # get mean USGS NGA-E
     nga_e_imt = nga_east_mean(mag, dep, dip, rake, rrup, vs30)
     #plt.loglog(nga_e_imt['per'], exp(nga_e_imt['sa']),'--' , lw=1.5, color=cs[5]) # for testing amp factors
@@ -63,11 +65,11 @@ for chim, rjb in zip(n_chim, rjbs):
     
     # adjust NGA-E from 3000 -> target
     nga_e_imt = adjust_gmm_with_nga_east(nga_e_imt, vs30)
-    '''
     
-    txt += ','.join((str(chim), str(rjb), str(exp(G90SEAimt['pga'][0][0])), str(exp(G90WAimt['pga'][0][0])), \
+    
+    txt += ','.join((str(chim), str(rhyp), str(rjb), str(exp(G90SEAimt['pga'][0][0])), str(exp(G90WAimt['pga'][0][0])), \
                      str(exp(AB06imt['pga'][0][0])), str(exp(Sea09imt_SS14['pga'][0][0])), str(exp(Sea09YCimt_SS14['pga'][0][0])), \
-                     str(exp(A12imt_SS14['pga'][0][0])), str(exp(Bea14imt['pga'][0][0])))) + '\n'
+                     str(exp(A12imt_SS14['pga'][0][0])), str(exp(Bea14imt['pga'][0][0])), str(exp(nga_e_imt['sa'][0])))) + '\n'
                      
 
 outfile = 'Chimney_RJB_GMPE_'+str('%0.0f' % vs30)+'.csv'
